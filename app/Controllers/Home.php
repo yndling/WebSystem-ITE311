449c6a12ea4f784e8ffdf4bd8807f0e6dffc85e9
yndling <jccagadas12@gmail.com>
2025-08-29 23:25:59 +0800

    Added Home controller, routes, and views for basic navigation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,23 +4,23 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-     public function template()
+    public function template(): string
     {
         return view('template');
     }
-     public function index()
+
+    public function index(): string
     {
         return view('index');
     }
 
-    public function about()
+    public function about(): string
     {
         return view('about');
     }
 
-    public function contact()
+    public function contact(): string
     {
         return view('contact');
     }
-    
 }
